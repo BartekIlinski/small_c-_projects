54054556ea2dec5038b40715de52fbd4bc0862ef
BartekIlinski <bartekilinski5@gmail.com>
2024-12-24 19:10:18 +0100

    .

---
+++ C++ Course/cpp2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //integer (whole number)
+    int age = 21;
+    int year = 2023;
+    int days = 7.5;
+
+    cout << days;
+
+    //duble (number including decimal)
+
+    double price = 10.99;
+    double gpa = 2.5;
+
+
+}
