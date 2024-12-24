5a21e25ad8c63d580477506cf17ee5fef1b13651
BartekIlinski <bartekilinski5@gmail.com>
2024-12-24 19:35:22 +0100

    .

--- C++ Course/cpp2.cpp
+++ C++ Course/cpp2.cpp
@@ -11,9 +11,30 @@ int main(){
     cout << days;
 
     //duble (number including decimal)
-
     double price = 10.99;
     double gpa = 2.5;
+    double temperature = 25.1;
+
+    cout << price;
+
+    //single character
+    char  grade = 'A';
+    char initial = 'C';
+    char dollarSign = '$';
+
+    //boolean (true or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    // string (objects that represents a sequence of text)
+    string name = "Bro";
+    string day = "Friday";
+    string food = "pizza";
+    string address = "123 Fake St.";
+
+    cout << "Hello" << name << '\n';
+    cout << "You are" << age << "Years old";
 
 
 }
