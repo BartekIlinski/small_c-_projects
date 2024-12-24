250cdc1139a66ad62e2a70bc40e5d5ceb775248c
BartekIlinski <bartekilinski5@gmail.com>
2024-12-24 19:10:11 +0100

    .

---
+++ C++ Course/cpp1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int x = 5;    
+    int y =6;
+    int sum = x + y;
+
+    cout << x << '\n';
+    cout << y << '\n';
+    cout << sum << '\n';
+}
