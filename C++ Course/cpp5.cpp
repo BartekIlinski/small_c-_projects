e63006ebc740e60aed482d98b5002d9d7fff76f9
BartekIlinski <bartekilinski5@gmail.com>
2024-12-28 18:32:22 +0100

    .

---
+++ C++ Course/cpp5.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+typedef std::string text_t;
+typedef int number_t;
+
+int main(){
+
+    text_t firstName = "bro";
+    number_t age = 21;
+
+    cout << firstName << '\n';
+    cout << age << "\n";
+
+}
